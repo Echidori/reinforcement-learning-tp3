842daf14a0b8bee4394c277769bdfc5c38f2a163
Emile <emile.merle@epita.fr>
2024-10-20 15:29:40 +0200

    feat: q-learning taxi implementation

--- taxi.py
+++ taxi.py
@@ -58,6 +58,10 @@ def play_and_train(env: gym.Env, agent: QLearningAgent, t_max=int(1e4)) -> float
 
         # Train agent for state s
         # BEGIN SOLUTION
+        agent.update(s, a, r, next_s)
+        total_reward += r
+        if done:
+            break
         # END SOLUTION
 
     return total_reward
